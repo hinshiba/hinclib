922e6cca0bdb9629b08d26ce15ba3c24cc26bbb2
hinshiba <105423175+hinshiba@users.noreply.github.com>
2025-07-05 15:30:11 +0900

    example: ex1を分かりやすいように変更

--- example/ex1.c
+++ example/ex1.c
@@ -8,10 +8,8 @@
 
 #include "datastructure/base/hinc_vec.h"
 
-vec_def(int);
-
 void print_int(int *n) {
-    printf("%d\n", *n);
+    printf("%d ", *n);
     return;
 }
 
@@ -20,27 +18,48 @@ void duble_int(int *n) {
     return;
 }
 
+int sum_int(const int *a, const int *b) { return *a + *b; }
+
+bool is_up15(const int *n) { return *n > 15; }
+
+/* 新しいvecの定義 */
+vec_def(int);
+
 int main(void) {
+    /* 新しいvecの宣言と初期化 */
     vec_int *vec = vec_int_from((int[]){1, 2, 3}, 3);
-    printf("%zu\n", vec->data_size);
-    printf("%zu\n", vec->len);
-    printf("%zu\n", vec->size);
+    printf("data_size: %zu\n", vec->data_size);
+    printf("len: %zu\n", vec->len);
+    printf("size: %zu\n", vec->size);
+    printf("\n");
 
-    printf("%d\n", *vec_int_get(vec, 0));
-    printf("%d\n", *vec_int_get(vec, 1));
-    printf("%d\n", *vec_int_get(vec, 2));
-    printf("%d\n", vec_int_pop(vec));
+    /* 安全な vec[i] */
+    printf("vec[0]: %d\n", *vec_int_get(vec, 0));
+    printf("vec[1]: %d\n", *vec_int_get(vec, 1));
+    printf("vec[2]: %d\n", *vec_int_get(vec, 2));
+    printf("\n");
+    /* 変更できるポインタの取得 */
+    *vec_int_get_mut(vec, 1) = 10;
 
+    /* pushとpop */
+    printf("pop: %d\n", vec_int_pop(vec));
     vec_int_push(vec, 100);
-    int *a = vec_int_get_mut(vec, 1);
-    *a = 10;
-    printf("%d\n", *vec_int_get(vec, 1));
-    printf("%d\n", *vec_int_get(vec, 2));
     printf("\n");
-    vec_int_foreach(vec, print_int);
-    printf("\n");
+
+    /* foreachの利用 */
     vec_int_foreach(vec, duble_int);
     vec_int_foreach(vec, print_int);
     printf("\n");
+
+    /* 条件を満たす要素だけの新しいvecを作成 */
+    vec_int *new_vec = vec_int_filter(vec, is_up15);
+    vec_int_foreach(new_vec, print_int);
+    printf("\n");
+
+    /* 累積的な計算 */
+    printf("sum: %d\n", vec_int_reduce(new_vec, sum_int, 0));
+    printf("\n");
+
     vec_free(vec);
+    vec_free(new_vec);
 }
